75ea6831a66a077e8bcab5cd5b68cd353c076f77
wli806 <wli806@aucklanduni.ac.nz>
2025-02-13 19:43:49 +1300

    Add click event to toggle animation on baubles in ex05.js

--- lab4 questions/lab 4.5/ex05.js
+++ lab4 questions/lab 4.5/ex05.js
@@ -1,3 +1,9 @@
 window.addEventListener("load", function() {
+    const baubles = document.querySelectorAll(".bauble");
 
+    baubles.forEach(bauble => {
+        bauble.addEventListener("click", function() {
+            this.classList.toggle("animated"); 
+        });
+    });
 });
